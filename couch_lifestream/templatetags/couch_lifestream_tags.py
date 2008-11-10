06ac3ccc7727630fbe7818edc45ad55ff0512bb6
Eric Florenzano <floguy@gmail.com>
2008-11-09 19:52:57 -0800

    Added some more convenience functions like twitterfying text, and getting the doc id.

--- couch_lifestream/templatetags/couch_lifestream_tags.py
+++ couch_lifestream/templatetags/couch_lifestream_tags.py
@@ -1,5 +1,8 @@
+import re
 from django import template
 from django.template.loader import render_to_string
+from django.template.defaultfilters import stringfilter
+from django.utils.safestring import mark_safe
 from couch_lifestream import db
 from copy import copy
 
@@ -28,4 +31,34 @@ class DisplayLifestreamItemNode(template.Node):
             context_with_item
         )
 
+def do_get_id_for_doc(parser, token):
+    try:
+        split = token.split_contents()
+    except ValueError:
+        raise template.TemplateSyntaxError('%r tag must be of format {%% %r DOC as CONTEXT_VAR_NAME %%}' % (token.contents.split()[0], token.contents.split()[0]))
+    if len(split) != 4:
+        raise template.TemplateSyntaxError('%r tag must be of format {%% %r DOC as CONTEXT_VAR_NAME %%}' % (token.contents.split()[0], token.contents.split()[0]))
+    return GetIdForDocNode(split[1], split[3])
+
+class GetIdForDocNode(template.Node):
+    def __init__(self, doc, context_var):
+        self.doc = template.Variable(doc)
+        self.context_var = context_var
+    
+    def render(self, context):
+        doc = self.doc.resolve(context)
+        context[self.context_var] = doc['_id']
+        return ''
+
+TWITTER_RE = re.compile('@(\S+)')
+
+def twitterfy(value):
+    return mark_safe(
+        TWITTER_RE.sub(r'<a href="http://twitter.com/\1">@\1</a>',value)
+    )
+twitterfy = stringfilter(twitterfy)
+
 register.tag('display_lifestream_item', do_display_lifestream_item)
+register.tag('get_id_for_doc', do_get_id_for_doc)
+
+register.filter('twitterfy', twitterfy)

df58564a0f57642b80fdda0f723769f77a574b28
Eric Florenzano <floguy@gmail.com>
2008-11-10 20:37:37 -0800

    Extracted out some of the parsing code to be reusable.

--- couch_lifestream/management/commands/fetch_lifestream_items.py
+++ couch_lifestream/management/commands/fetch_lifestream_items.py
@@ -12,81 +12,73 @@ except ImportError:
 
 TWITTER_USERNAME = getattr(settings, 'TWITTER_USERNAME', None)
 
+### HELPER FUNCTIONS ###
+
+def parse_json(url, item_type, callback, discriminator='id', list_key=None):
+    print "Fetching %s items" % (item_type,)
+    fetched = urlopen(url).read()
+    data = json.loads(fetched)
+    if list_key:
+        data = data[list_key]
+    map_fun = 'function(doc) { emit(doc.id, null); }'
+    for item in map(callback, data):
+        item['item_type'] = item_type
+        if len(db.query(map_fun, key=item[discriminator])) == 0:
+            db.create(item)
+    print "%s items fetched" % (item_type,)
+
+def parse_feed(url, item_type, discriminator='id'):
+    import feedparser
+    print "Fetching %s items" % (item_type,)
+    d = feedparser.parse(url)
+    map_fun = 'function(doc) { emit(doc.%s, null); }' % (discriminator,)
+    for item in map(dict, d['entries']):
+        item['item_type'] = item_type
+        item['couch_lifestream_date'] = datetime.datetime.fromtimestamp(
+            time.mktime(item['updated_parsed']))
+        if len(db.query(map_fun, key=item[discriminator])) == 0:
+            for (key, val) in item.items():
+                if 'parsed' in key:
+                    del item[key]
+                elif isinstance(val, datetime.datetime):
+                    item[key] = val.isoformat()
+                elif isinstance(val, datetime.date):
+                    item[key] = val.isoformat()
+            db.create(item)
+    print "%s items fetched" % (item_type,)
+
+### PER-SERVICE FUNCTIONS ###
+
 def fetch_twitter_items():
     from dateutil.parser import parse
-    print "Fetching Twitter items"
-    fetched = urlopen('http://twitter.com/statuses/user_timeline.json?id=%s' % (
-        TWITTER_USERNAME,)).read()
-    data = json.loads(fetched)
-    map_fun = 'function(doc) { emit(doc.id, null); }'
-    for item in data:
-        item['item_type'] = 'twitter'
+    def callback(item):
         item['couch_lifestream_date'] = parse(item['created_at']).isoformat()
-        if len(db.query(map_fun, key=item['id'])) == 0:
-            db.create(item)
-    print "Twitter items fetched"
+        return item
+    url = 'http://twitter.com/statuses/user_timeline.json?id=%s' % (TWITTER_USERNAME,)
+    parse_json(url, 'twitter', callback)
 
 POWNCE_USERNAME = getattr(settings, 'POWNCE_USERNAME', None)
 
 def fetch_pownce_items():
-    print "Fetching Pownce items"
-    fetched = urlopen('http://api.pownce.com/2.0/note_lists/%s.json' % (
-        TWITTER_USERNAME,)).read()
-    data = json.loads(fetched)['notes']
-    map_fun = 'function(doc) { emit(doc.id, null); }'
-    for item in data:
-        item['item_type'] = 'pownce'
+    url = 'http://api.pownce.com/2.0/note_lists/%s.json' % (POWNCE_USERNAME,)
+    def callback(item):
         y, month, d, h, m, s, wd, jd, ds = time.gmtime(item['timestamp'])
         couch_lifestream_date = datetime.datetime(y, month, d, h, m, s)
         item['couch_lifestream_date'] = couch_lifestream_date.isoformat()
-        if len(db.query(map_fun, key=item['id'])) == 0:
-            db.create(item)
-    print "Pownce items fetched"
+        return item
+    parse_json(url, 'pownce', callback, list_key='notes')
 
 REDDIT_USERNAME = getattr(settings, 'REDDIT_USERNAME', None)
 
 def fetch_reddit_items():
-    import feedparser
-    print "Fetching Reddit items"
-    d = feedparser.parse('http://www.reddit.com/user/%s/comments/.rss' % (
-        REDDIT_USERNAME,))
-    map_fun = 'function(doc) { emit(doc.link, null); }'
-    for item in map(dict, d['entries']):
-        item['item_type'] = 'reddit-comment'
-        item['couch_lifestream_date'] = datetime.datetime.fromtimestamp(
-            time.mktime(item['updated_parsed']))
-        if len(db.query(map_fun, key=item['link'])) == 0:
-            for (key, val) in item.items():
-                if 'parsed' in key:
-                    del item[key]
-                elif isinstance(val, datetime.datetime):
-                    item[key] = val.isoformat()
-                elif isinstance(val, datetime.date):
-                    item[key] = val.isoformat()
-            db.create(item)
-    print "Reddit items fetched"
+    reddit_url = 'http://www.reddit.com/user/%s/comments/.rss' % (REDDIT_USERNAME,)
+    parse_feed(reddit_url, 'reddit-comment', discriminator='link')
 
 FLICKR_USER_ID = getattr(settings, 'FLICKR_USER_ID', None)
 
 def fetch_flickr_items():
-    import feedparser
-    print "Fetching Flickr items"
-    d = feedparser.parse('http://api.flickr.com/services/feeds/photos_public.gne?format=atom&id=%s' % (FLICKR_USER_ID,))
-    map_fun = 'function(doc) { emit(doc.id, null); }'
-    for item in map(dict, d['entries']):
-        item['item_type'] = 'flickr'
-        item['couch_lifestream_date'] = datetime.datetime.fromtimestamp(
-            time.mktime(item['updated_parsed']))
-        if len(db.query(map_fun, key=item['id'])) == 0:
-            for (key, val) in item.items():
-                if 'parsed' in key:
-                    del item[key]
-                elif isinstance(val, datetime.datetime):
-                    item[key] = val.isoformat()
-                elif isinstance(val, datetime.date):
-                    item[key] = val.isoformat()
-            db.create(item)
-    print "Flickr items fetched"
+    flickr_url = 'http://api.flickr.com/services/feeds/photos_public.gne?format=atom&id=%s' % (FLICKR_USER_ID,)
+    parse_feed(flickr_url, 'flickr')
 
 class Command(NoArgsCommand):
     help = 'Fetch the latest lifestream items and insert them into CouchDB.'
